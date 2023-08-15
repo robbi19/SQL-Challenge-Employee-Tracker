1966bc8d5c928c5de9240d68d3bd71ea612057c2
rob <collinsduo1@gmail.com>
2023-08-15 14:02:13 -0500

    add employee table and made sure everthing was capitalize

--- db/schema.sql
+++ db/schema.sql
@@ -11,4 +11,24 @@ CREATE TABLE department (
     PRIMARY KEY(id)
 );
 
+/*roles table area*/
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id) REFERENCES department(id)
+);
 
+/*  table area*/
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(role_id) REFERENCES roles(id),
+    FOREIGN KEY(manager_id) REFERENCES employee(id)
+);
